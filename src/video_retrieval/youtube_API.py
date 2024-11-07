d2f9ed6c3479d4816e893f3df198c88053bd5eb1
breadpeel <brad.passage@gmail.com>
2024-11-06 19:50:27 -0600

    Added distilbert and YouTube API

---
+++ src/video_retrieval/youtube_API.py
@@ -0,0 +1,17 @@
+import googleapiclient.discovery
+import os
+
+def search_youtube(query, max_results=5):
+    """Searches YouTube for videos based on the given query."""
+    api_key = os.getenv("AIzaSyAV9j3x5eSpKSNqELGYg5fZ5xzKNV-auYs")
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
+
+    request = youtube.search().list(
+        part="snippet",
+        q=query,
+        type="video",
+        maxResults=max_results
+    )
+    response = request.execute()
+
+    return response.get('items', [])
