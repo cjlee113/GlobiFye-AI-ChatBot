3fa7cf28a259cabb3fd36a72eaa7cf9e8e090e85
breadpeel <brad.passage@gmail.com>
2024-11-11 11:18:13 -0600

    Modified config_loader.py

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -5,5 +5,5 @@ def load_config():
     """Loads configuration settings and API keys."""
     load_dotenv()
     return {
-        'YOUTUBE_API_KEY': os.getenv("YOUTUBE_API_KEY")
+        'YOUTUBE_API_KEY': os.getenv("AIzaSyAV9j3x5eSpKSNqELGYg5fZ5xzKNV-auYs")
     }
