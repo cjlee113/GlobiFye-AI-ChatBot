d4379471a37bdaa0e5543757751d02e168c338a2
FrankLeFebvre2023 <70653559+FrankLeFebvre2023@users.noreply.github.com>
2024-11-12 10:21:28 -0500

    Completed video selector function. Selects top 10 videos based on highest relevance score

---
+++ src/video_retrieval/video_selector.py
@@ -0,0 +1,18 @@
+from transformers import DistilBertTokenizer, DistilBertModel
+from youtube_API import search_youtube
+from ..nlp_filtering.distilbert_filter import score_similarity, load_model
+
+def select_videos(query):
+    """Automatically selects videos based on relevance to resume keywords"""
+    tokenizer, model = load_model()
+    videos = search_youtube(query)
+    selected_videos = []
+    threshold = .95
+    while len(select_videos) < 10:
+        relevant_videos = score_similarity(query, videos, tokenizer, model, threshold)
+        if len(relevant_videos) >= 10:
+            selected_videos.append(relevant_videos[:10])
+        else:
+            selected_videos.append(relevant_videos)
+        threshold -= .05
+    return selected_videos
